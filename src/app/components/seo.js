64a094d1f2336bb30e8e01bad8f2e46c97d27406
Haziq Hapiz <zyqq.dev@gmail.com>
2023-12-22 21:01:31 +0800

    add SEO

---
+++ src/app/components/seo.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+function SEO({ title, description, imageUrl, url }) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      {/* Open Graph meta tags */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={imageUrl} />
+      <meta property="og:url" content={url} />
+      {/* Twitter Card meta tags (optional) */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={imageUrl} />
+    </Head>
+  );
+}
+
+export default SEO;
